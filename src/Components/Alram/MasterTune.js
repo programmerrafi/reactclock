869a3aa93005eedc233fc8592d78634bf4c097b8
mdrafi10 <79692008+mdrafi10@users.noreply.github.com>
2021-10-11 02:38:47 +0600

    Update MasterTune.js

--- src/Components/Alram/MasterTune.js
+++ src/Components/Alram/MasterTune.js
@@ -1,27 +1,45 @@
 import React from "react";
 import game from "./music/music.mp3";
-const MasterTune = ({ giveaway, deleteData, id, label }) => {
+const MasterTune = ({
+  giveaway,
+  deleteData,
+  id,
+  label,
+  audiPlay,
+  onOff,
+  handleOnOff,
+  editTog,
+}) => {
   return (
     <>
       <div className="deadline">
-        <div className="icon_control">
-          <div className="icon_delete" title="stop alarm">
-            <i
-              className="fas fa-times-circle"
-              onClick={() => deleteData(id)}
-            ></i>
+        {editTog ? null : (
+          <div className="icon_control">
+            <div className="icon_delete" title="delete">
+              <i
+                className="fas fa-times-circle"
+                onClick={() => deleteData(id)}
+              ></i>
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="deadline_info">
-          <div className="alarm_tune">
-            <h4 className="giveaway">{giveaway}</h4>
-            <audio src={game} autoPlay></audio>
-          </div>
+          {giveaway}
+          {audiPlay ? null : onOff ? null : <audio src={game} autoPlay></audio>}
+
           <div className="massage" style={{ textAlign: "center" }}>
             <h2>{label}</h2>
             {/* <h3 style={{ color: "#c07718" }}>{label}</h3> */}
           </div>
+          {/* swtich btn start */}
+          <div
+            className={`button_mobile ${onOff ? "" : "bg"}`}
+            onClick={handleOnOff}
+          >
+            <span className={`switch ${onOff ? "slide" : ""}`}></span>
+          </div>
+          {/* swtich btn end */}
         </div>
       </div>
     </>
